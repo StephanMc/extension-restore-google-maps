be40eed08be30429abe3e3a048e351318a13a789
Stephane Kouadio <stephan.kouadio@gmail.com>
2024-04-15 02:04:03 +0200

    fix google flights
    When searching for terms "Google Flights", the link in result appended "gl=ch". See #1

--- background.js
+++ background.js
@@ -7,6 +7,10 @@ function isGoogleSearchUrl(urlObject) {
   return urlObject.origin.startsWith("https://www.google.") && urlObject.pathname === "/search";
 }
 
+function isGoogleFlightUrl(urlObject) {
+  return urlObject.origin.startsWith("https://www.google.") && urlObject.pathname.startsWith("/travel/flights");
+}
+
 chrome.webNavigation.onBeforeNavigate.addListener(
   function (details) {
     const isDocumentLoaded =
@@ -17,6 +21,13 @@ chrome.webNavigation.onBeforeNavigate.addListener(
 
     if (isDocumentLoaded && isGoogleSearchUrl(url) && !url.search.includes("gl=ch")) {
       url.searchParams.set("gl", "ch");
+
+      chrome.tabs.update(details.tabId, { url: url.toString() }, function () {});
+    } else if (isDocumentLoaded && isGoogleFlightUrl(url) && url.search.toLowerCase().includes("gl=ch")) {
+      // When searching "Google Flights" on search, it appended by default "GL=ch" on flight results, making prices to appear in CHF.
+      // Fix it by removing the parameter, to use the default currency.
+      url.searchParams.delete("gl");
+      url.searchParams.delete("GL");
 
       chrome.tabs.update(details.tabId, { url: url.toString() }, function () {});
     }
